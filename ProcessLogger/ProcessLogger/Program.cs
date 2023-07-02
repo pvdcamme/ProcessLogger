5949e1b54800c99baba8c63955f1ec8e4fcb39c8
pvdcamme <pvdcamme@gmail.com>
2023-07-02 13:34:34 +0000

    read out value more once (I.e. does it work?)

--- ProcessLogger/ProcessLogger/Program.cs
+++ ProcessLogger/ProcessLogger/Program.cs
@@ -3,14 +3,23 @@
 
 /** A very early prototype to gather log the process run-times
 */
-foreach (var proc in ProcessLogger.RunningProcesses())
+
+var processes = ProcessLogger.RunningProcesses();
+for(int ctr = 0; ctr < 10; ++ctr)
 {
-    try
+    foreach (var proc in processes)
     {
-        Console.WriteLine(proc.Name + " -- " + proc.GetProcessorTime());
+        try
+        {
+            float time = proc.GetProcessorTime();
+            if (time > 0){
+                Console.WriteLine(proc.Name + " -- " + time);
+            }
+        }
+        catch (System.InvalidOperationException)
+        {
+            Console.WriteLine(proc.Name + " does not support getting processor Time");
+        }
     }
-    catch (System.InvalidOperationException)
-    {
-        Console.WriteLine(proc.Name + " does not support getting processor Time");
-    }
+    Thread.Sleep(1000);
 }
