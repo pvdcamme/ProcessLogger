7d6914498b81e76e6adc80cdbb379a70a14870da
pvdcamme <pvdcamme@gmail.com>
2023-07-02 13:45:59 +0000

    cleanup: Regactor the exception handling to the ProcessLogger

--- ProcessLogger/ProcessLogger/ProcessLogger.cs
+++ ProcessLogger/ProcessLogger/ProcessLogger.cs
@@ -1,31 +1,50 @@
 
 using System.Diagnostics;
+using System.Net.Mail;
 
 /** Logs results from a single Process.
 Provides factory methods to generalize easily create an instance for the 
 whole system.()
 */
-class ProcessLogger{
+class ProcessLogger
+{
 
-    public static IEnumerable<ProcessLogger> RunningProcesses(){
+    public static IEnumerable<ProcessLogger> RunningProcesses()
+    {
         PerformanceCounterCategory cat = new("Process");
         List<ProcessLogger> result = new();
 
-        foreach(string name in cat.GetInstanceNames()){
+        foreach (string name in cat.GetInstanceNames())
+        {
             result.Add(new ProcessLogger(name));
         }
         return result;
     }
 
-    public string Name {get;}    
-    private PerformanceCounter Counter {get;}
+    public string Name { get; }
+    private PerformanceCounter Counter { get; }
+    private int ReadFailures;
+    private int ReadAttempt;
 
-    private ProcessLogger(string name){
+    private ProcessLogger(string name)
+    {
         this.Name = name;
         this.Counter = new("Process", "% Processor Time", name, true);
     }
 
-    public float GetProcessorTime(){
-        return Counter.NextValue();
+    public float GetProcessorTime()
+    {
+        float result = 0;
+        ReadAttempt++;
+        try
+        {
+            result = Counter.NextValue();
+        }
+        catch (InvalidOperationException)
+        {
+            ReadFailures++;
+            Console.WriteLine("Failed to read " + Name + " " + ReadFailures + " / " + ReadAttempt);
+        }
+        return result;
     }
 }
--- ProcessLogger/ProcessLogger/Program.cs
+++ ProcessLogger/ProcessLogger/Program.cs
@@ -9,16 +9,9 @@ for(int ctr = 0; ctr < 10; ++ctr)
 {
     foreach (var proc in processes)
     {
-        try
-        {
-            float time = proc.GetProcessorTime();
-            if (time > 0){
-                Console.WriteLine(proc.Name + " -- " + time);
-            }
-        }
-        catch (System.InvalidOperationException)
-        {
-            Console.WriteLine(proc.Name + " does not support getting processor Time");
+        float time = proc.GetProcessorTime();
+        if (time > 0){
+            Console.WriteLine(proc.Name + " -- " + time);
         }
     }
     Thread.Sleep(1000);
