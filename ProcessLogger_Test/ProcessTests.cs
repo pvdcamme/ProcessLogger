a5abdff69b0307657955394363aec3562c428fbe
pvdcamme <pvdcamme@gmail.com>
2023-07-09 19:12:52 +0000

    Check the logged values

--- ProcessLogger_Test/ProcessTests.cs
+++ ProcessLogger_Test/ProcessTests.cs
@@ -1,13 +1,26 @@
 namespace ProcessLogger_Test
 {
     using ProcessLogger;
-    public class PocressLogger
+    using System.Linq.Expressions;
+
+    public class ProcessTests
     {        
         [Fact]
         public void ExpectsSomeProcesses()
         {
             var current_processes = ProcessLogger.RunningProcesses();
             Assert.NotEmpty(current_processes);
+        }
+
+
+        [Fact]
+        public void CheckHasNextValues()
+        {
+            var current_processes = ProcessLogger.RunningProcesses();
+            foreach(ProcessLogger aLogger in ProcessLogger.RunningProcesses())
+            {
+                Assert.True(aLogger.GetProcessorTime() >= 0, "Postive CPU time expexted");
+            }            
         }
     }
 }
