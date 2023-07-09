4037e5f5cc688ad8e20d1eac496da55a5ccd8784
pvdcamme <pvdcamme@gmail.com>
2023-07-09 19:15:35 +0000

    cleanup: reformat

--- ProcessLogger_Test/ProcessTests.cs
+++ ProcessLogger_Test/ProcessTests.cs
@@ -4,23 +4,22 @@ namespace ProcessLogger_Test
     using System.Linq.Expressions;
 
     public class ProcessTests
-    {        
+    {
         [Fact]
         public void ExpectsSomeProcesses()
         {
-            var current_processes = ProcessLogger.RunningProcesses();
-            Assert.NotEmpty(current_processes);
+            var currentProcesses = ProcessLogger.RunningProcesses();
+            Assert.NotEmpty(currentProcesses);
         }
 
 
         [Fact]
         public void CheckHasNextValues()
         {
-            var current_processes = ProcessLogger.RunningProcesses();
-            foreach(ProcessLogger aLogger in ProcessLogger.RunningProcesses())
+            foreach (ProcessLogger aLogger in ProcessLogger.RunningProcesses())
             {
                 Assert.True(aLogger.GetProcessorTime() >= 0, "Postive CPU time expexted");
-            }            
+            }
         }
     }
 }
