45bff5ad056cf3f3d7b19a5640597b9814798585
pvdcamme <pvdcamme@gmail.com>
2023-07-15 22:41:32 +0000

    feature: Start work on persistent storage

---
+++ ProcessLogger/ProcessLogger/FileLog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FileLog
+{
+    public class FileLog
+    {
+        public static FileLog InDocuments(string Name)
+        {
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string finalDest = Path.Combine(docPath, Name);
+            return new FileLog(finalDest);
+        }
+        private string SavePath { get; }
+
+        public FileLog(string SavePath) 
+        {
+            this.SavePath = SavePath;
+        }
+
+        public void AddEntry(string key, float val)
+        {
+            using(StreamWriter output = new StreamWriter(SavePath))
+            {
+                output.WriteLine($"{key} :: {val}");
+            }
+        }
+    }
+}
---
+++ ProcessLogger_Test/FileLogTests.cs
@@ -0,0 +1,23 @@
+﻿using FileLog;
+
+namespace ProcessLogger.Test
+{
+    public class FileLogTests
+    {
+
+        [Fact]
+        public void CreateFromDocuments()
+        {
+            try
+            {
+                FileLog.FileLog.InDocuments("test.txt");
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Can't create");
+
+            }
+        }
+
+    }
+}
