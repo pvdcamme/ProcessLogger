6eda8a43bda67dabcf8eac4f7c09b2f53548ec2e
pvdcamme <pvdcamme@gmail.com>
2023-07-16 22:19:08 +0000

    feature: Adding more tests

--- ProcessLogger_Test/FileLogTests.cs
+++ ProcessLogger_Test/FileLogTests.cs
@@ -2,7 +2,8 @@
 
 namespace ProcessLogger.Test
 {
-    using FileLog;
+    using FileLog;    
+
     public class FileLogTests
     {
 
@@ -10,6 +11,7 @@ namespace ProcessLogger.Test
         [Fact]
         public void CreateFromDocuments()
         {
+            // Can we actually instantiatie a Logger.
             try
             {
                 var logger = FileLog.InDocuments("test.txt");
@@ -25,6 +27,7 @@ namespace ProcessLogger.Test
         [Fact]
         public void AddSingleEntry()
         {
+            // The most basic entry-test, just a single entry.
             var logger = FileLog.InDocuments("test.txt");
             logger.Reset();
             logger.AddEntry("test", 1f);
@@ -37,5 +40,33 @@ namespace ProcessLogger.Test
             Assert.Equal("test", soleResult.Key);
             Assert.Equal(1f, soleResult.Value);
         }
+
+        [Fact]
+        public void AddMultipleEntries()
+        {
+            // Slightly more complicated test. Allows to also verify the timestamps.
+            var logger = FileLog.InDocuments("test.txt");
+            logger.Reset();
+            float[] toAdd = { 1f, 23f, 45f, 0f, -12f, 100000f };
+            const string keyName = "test";
+            foreach(var entry in toAdd)
+            {
+                logger.AddEntry(keyName, entry);
+            }
+
+            int entryCount = 0;
+            long previousMoment = long.MinValue;
+            foreach (var entry in logger.GetEntries())
+            {
+                Assert.Equal(keyName, entry.Key);
+                Assert.Equal(toAdd[entryCount], entry.Value);
+                Assert.InRange(entry.When, previousMoment, long.MaxValue);
+
+                previousMoment = entry.When;
+                entryCount++;
+            }
+            Assert.Equal(toAdd.Length, entryCount);
+        }
     }
+
 }
